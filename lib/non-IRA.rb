784e93b2ef472a3e74502ae3fe95d24c75501727
RichardReed <richardreed1129@yahoo.com>
2015-09-26 18:33:53 -0400

    Adding rspec tests for non-IRA class

--- lib/non-IRA.rb
+++ lib/non-IRA.rb
@@ -11,61 +11,64 @@ require_relative 'IncomeTax'
 
 class NonIRA
 
-    def initialize
-        @config_hash = ConfigFile.new
-        @current_year = @config_hash.config['starting_year']
-        @income_in = IncomeCalc.new
-        @expenses_in = ExpenseCalc.new
-        @income_tax_in = IncomeTax.new
+  def initialize
+    @config_hash = ConfigFile.new
+    @current_year = @config_hash.config['starting_year']
+    @income_in = IncomeCalc.new
+    @expenses_in = ExpenseCalc.new
+    @income_tax_in = IncomeTax.new
+  end
+
+  def sav_spend(this_year)
+    income_expenses = @income_in.gross_income(this_year) -
+      @expenses_in.gross_exp(this_year)
+    save_spend = @income_in.gross_income(this_year) -
+      @expenses_in.gross_exp(this_year) -
+      @income_tax_in.income_tax(this_year)
+    income_tax = -@income_tax_in.income_tax(this_year)
+
+    if this_year < 60.is_year
+      return save_spend
+    elsif income_expenses > 0
+      return  save_spend
+    else
+      return income_tax
     end
+  end
 
-    def sav_spend(this_year)
-        income_expenses = @income_in.gross_income(this_year) -
-            @expenses_in.gross_exp(this_year)
-        save_spend = @income_in.gross_income(this_year) -
-            @expenses_in.gross_exp(this_year) -
-            @income_tax_in.income_tax(this_year)
-        income_tax = -@income_tax_in.income_tax(this_year)
+  def non_ira_account(final_year)
+    this_year = @current_year
+    non_ira_account_value = initial_non_ira_account_value
+    ira_to_non_ira_xfer = @config_hash.config['ira_to_non_ira_xfer']
+    interest = @config_hash.config['savings_interest_rate'] / 100.0
 
-        if this_year < 60.is_year
-            return save_spend
-        elsif income_expenses > 0
-            return  save_spend
-        else
-            return income_tax
-        end
-    end
+    this_year.upto(final_year) do |year|
+      if year.is_age < 60
+        non_ira_account_value = ((non_ira_account_value +
+                      sav_spend(year)) * (1 + interest)).round
+      else
+        non_ira_account_value = ((non_ira_account_value +
+                      sav_spend(year) + ira_to_non_ira_xfer) *
+                      (1 + interest)).round
+      end
+    end  # do
+    return non_ira_account_value
+  end
 
-    def non_ira_account(final_year)
-        this_year = @current_year
-        non_ira_account_value = initial_non_ira_account_value
-        ira_to_non_ira_xfer = @config_hash.config['ira_to_non_ira_xfer']
-        interest = @config_hash.config['savings_interest_rate'] / 100.0
+ private
 
-        this_year.upto(final_year) do |year|
-            if year.is_age < 60
-                non_ira_account_value = ((non_ira_account_value +
-                                sav_spend(year)) * (1 + interest)).round
-            else
-                non_ira_account_value = ((non_ira_account_value +
-                                sav_spend(year) + ira_to_non_ira_xfer) *
-                                (1 + interest)).round
-            end
-        end  # do
-        return non_ira_account_value
-    end
+  def initial_non_ira_account_value
+    starting_non_ira = @config_hash.config['starting_non_ira']
+    starting_non_ira_disc = @config_hash.config['starting_savings_discount']
 
-  private
+    return (starting_non_ira * starting_non_ira_disc / 100) - starting_debt
+  end
 
-    def initial_non_ira_account_value
-      starting_non_ira = @config_hash.config['starting_non_ira']
-      starting_non_ira_disc = @config_hash.config['starting_savings_discount']
-      debt_amount = @config_hash.config['debt_amount']
-      debt_interest = @config_hash.config['debt_interest']
-      debt_years = @config_hash.config['debt_years']
+  def starting_debt 
+    debt_amount = @config_hash.config['debt_amount']
+    debt_interest = @config_hash.config['debt_interest']
+    debt_years = @config_hash.config['debt_years']
 
-      starting_debt = debt_amount * (1 + (debt_years * debt_interest / 200.0))
-      initial_non_ira_account = (starting_non_ira * starting_non_ira_disc / 
-                                100) - starting_debt
-    end
+    return debt_amount * (1 + (debt_years * debt_interest / 200.0))
+  end
 end
---
+++ rspec/non-IRA_spec.rb
@@ -0,0 +1,154 @@
+require_relative 'spec_helper'
+
+describe "save_spend (this year)" do
+  describe "calculates the year's non-IRA savings/spending" do
+    context "with income exceeding expenses and taxes by $500" do
+      before (:each) do
+        config_file = ConfigFile.new
+        config_file.set_config_override ({ "birth_year" => 1960 })
+        @non_ira = NonIRA.new
+        allow_any_instance_of(IncomeCalc).to receive(:gross_income).
+                 and_return(2000)
+        allow_any_instance_of(ExpenseCalc).to receive(:gross_exp).
+                 and_return(1000)
+        allow_any_instance_of(IncomeTax).to receive(:income_tax).
+                 and_return(400)
+      end
+      it "returns $600 if age is less than 60" do
+        expect(@non_ira.sav_spend(2019)).to eq(600)
+      end
+      it "returns $600 if age is >=60 and income > expenses" do
+        expect(@non_ira.sav_spend(2020)).to eq(600)
+      end
+      it "returns -$400 if age is >=60 and expenses > income" do
+        allow_any_instance_of(IncomeCalc).to receive(:gross_income).
+                 and_return(2000)
+        allow_any_instance_of(ExpenseCalc).to receive(:gross_exp).
+                 and_return(2001)
+        expect(@non_ira.sav_spend(2020)).to eq(-400)
+      end
+    end
+  end
+end
+describe "non-ira_account" do
+  describe "calculates the non-IRA account value in the final year" do
+    context "with final age <60 and initial non-IRA value of $1000" do
+      before (:each) do
+        @config_file = ConfigFile.new
+        @config_file.set_config_override ({
+          "starting_year" => 2010,
+          "birth_year" => 1960, 
+          "ira_to_non_ira_xfer" => 0,
+          "savings_interest_rate" => 0 
+          })
+        @non_ira = NonIRA.new
+        allow(@non_ira).to receive(:initial_non_ira_account_value).
+             and_return(1000)
+        allow(@non_ira).to receive(:sav_spend).and_return(0)
+      end
+      it "returns $1000 the 10th year with $0 annual spending" do
+        expect(@non_ira.non_ira_account(2019)).to eq(1000)
+      end
+      it "returns $2000 the 10th year with $100 annual savings" do
+        allow(@non_ira).to receive(:sav_spend).and_return(100)
+        expect(@non_ira.non_ira_account(2019)).to eq(2000)
+      end
+      it "returns $1630 the 10th year with 5% interest" do
+        @config_file.config["savings_interest_rate"] = 5
+        expect(@non_ira.non_ira_account(2019)).to eq(1630)
+      end
+    end
+    context "with initial age >=60 and initial non-IRA value of $1000" do
+      before (:each) do
+        @config_file = ConfigFile.new
+        @config_file.set_config_override ({
+          "starting_year" => 2021,
+          "birth_year" => 1960, 
+          "ira_to_non_ira_xfer" => 0,
+          "savings_interest_rate" => 0, 
+          "ira_to_non_ira_xfer" => 0
+          })
+        @non_ira = NonIRA.new
+        allow(@non_ira).to receive(:initial_non_ira_account_value).
+             and_return(1000)
+        allow(@non_ira).to receive(:sav_spend).and_return(0)
+      end
+      it "returns $1000 the 10th year with $0 annual spending" do
+        expect(@non_ira.non_ira_account(2030)).to eq(1000)
+      end
+      it "returns $2000 the 10th year with $100 annual savings" do
+        allow(@non_ira).to receive(:sav_spend).and_return(100)
+        expect(@non_ira.non_ira_account(2030)).to eq(2000)
+      end
+      it "returns $1630 the 10th year with 5% interest" do
+        @config_file.config["savings_interest_rate"] = 5
+        expect(@non_ira.non_ira_account(2030)).to eq(1630)
+      end
+      it "returns $2000 the 10th year with $100 IRA to non-IRA xfer" do
+        @config_file.config["ira_to_non_ira_xfer"] = 100
+        expect(@non_ira.non_ira_account(2030)).to eq(2000)
+      end
+    end
+    context "with initial age <60, final age >60 and initial value $1000" do
+      before (:each) do
+        @config_file = ConfigFile.new
+        @config_file.set_config_override ({
+          "starting_year" => 2016,
+          "birth_year" => 1960, 
+          "ira_to_non_ira_xfer" => 0,
+          "savings_interest_rate" => 0, 
+          "ira_to_non_ira_xfer" => 100
+          })
+        @non_ira = NonIRA.new
+        allow(@non_ira).to receive(:initial_non_ira_account_value).
+             and_return(1000)
+        allow(@non_ira).to receive(:sav_spend).and_return(0)
+      end
+      it "returns $1600 the 10th year with $100 IRA to non-IRA "+
+         "xfer for 6 yrs after age >60" do
+        expect(@non_ira.non_ira_account(2025)).to eq(1600)
+      end
+    end
+  end
+end
+describe "initial_non-ira_account_value" do
+  describe "calculates the starting value of the non-IRA account" do
+    context "with starting non-IRA amount of $1000" do
+      before (:each) do
+        @config_file = ConfigFile.new
+        @config_file.set_config_override ({
+          "starting_non_ira" => 1000,
+          "starting_savings_discount" => 100,
+          "debt_amount" => 0,
+          "debt_interest" => 0,
+          "debt_years" => 0 
+          })
+        @non_ira = NonIRA.new
+      end
+      it "returns $1000 with $0 debt and 100% not discounted" do
+        expect(@non_ira.send(:initial_non_ira_account_value)).to eq(1000)
+      end
+      it "returns $800 with $0 debt and 80% not discounted" do
+        @config_file.config["starting_savings_discount"] = 80
+        expect(@non_ira.send(:initial_non_ira_account_value)).to eq(800)
+      end
+      it "returns $800 with $200 debt, 0% interest for 10 years" do
+        @config_file.config["debt_amount"] = 200
+        @config_file.config["debt_years"] = 10
+        expect(@non_ira.send(:initial_non_ira_account_value)).to eq(800)
+      end
+      it "returns $700 with $200 debt, 10% interest for 10 years" do
+        @config_file.config["debt_amount"] = 200
+        @config_file.config["debt_years"] = 10
+        @config_file.config["debt_interest"] = 10
+        expect(@non_ira.send(:initial_non_ira_account_value)).to eq(700)
+      end
+      it "returns $770 with $200 debt, 10% interest for 3 years" do
+        @config_file.config["debt_amount"] = 200
+        @config_file.config["debt_years"] = 3
+        @config_file.config["debt_interest"] = 10
+        expect(@non_ira.send(:initial_non_ira_account_value)).to eq(770)
+      end
+    end
+  end
+end
